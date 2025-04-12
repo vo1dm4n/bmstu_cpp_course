911681108953dcd3c4c175edb413019356192579
vo1dm4n <a67461590@gmail.com>
2025-04-12 11:20:14 +0300

    Update base_node_let.h

--- tasks/bmstu_lets/task_let_2_2/base_node_let.h
+++ tasks/bmstu_lets/task_let_2_2/base_node_let.h
@@ -41,24 +41,66 @@ struct ForwardListNode
 template <typename T>
 void create_list(ForwardListNode<T>*& head, const std::vector<T>& data)
 {
+	delete_list(head);
+	if (data.empty()) return;
+	head = new ForwardListNode<T>* current = head;
+	for (size_t i = 1; i < data.size(); ++i){
+		current->next = new ForwardListNode<T>(data[i]);
+		current = current->next;
+	}
 }
 
 template <typename T>
 void delete_list(ForwardListNode<T>*& head)
 {
+	while (head != nullptr){
+		ForwardlistNode<T>* temp = head;
+		head = head->next;
+		delete temp;
+	}
 }
 
 template <typename T>
 void print_list(const ForwardListNode<T>* head, const std::ostream& os)
 {
+	while (current != nullptr){
+		os << current->data;
+		if (current->next != nullptr)
+			os << " -> ";
+		current = current->next;
+	}
+	os << '\n';
 }
 
 template <typename T>
 void reverse_list(ForwardListNode<T>*& head)
 {
+	ForwardListNode<T>* prev = nullptr;
+	ForwardListNode<T>* current = head;
+	ForwardListNode<T>* next = nullptr;
+
+	while (current != nullptr){
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	head = prev;
 }
 
 template <typename T>
 void remove_duplicates(ForwardListNode<T>*& head)
 {
+	if (head == nullptr) return;
+	ForwardListNode<T>* current = head;
+	while (current->next != nullptr){
+		if (current->data == current->next->data){
+			ForwardListNode<T>* temp = current->next;
+			current->next = current->next->next;
+			delete temp;
+		}
+		else {
+			current = current->next;
+		}
+	}
 }
